086e7a0091f75b4883eba694e824569899d09b64
Benoit Guerout <bguerout@gmail.com>
2011-06-14 22:03:31 +0200

    Add bulk creation for handlers

--- src/main/java/fr/xevents/core/fetcher/FetcherHandlerFactory.java
+++ src/main/java/fr/xevents/core/fetcher/FetcherHandlerFactory.java
@@ -1,18 +1,33 @@
 package fr.xevents.core.fetcher;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import fr.xevents.core.EventRepository;
 import fr.xevents.core.User;
+import fr.xevents.core.UserResolver;
 
 public class FetcherHandlerFactory {
 
     private final EventRepository eventRepository;
+    private final UserResolver userResolver;
 
-    public FetcherHandlerFactory(EventRepository eventRepository) {
+    public FetcherHandlerFactory(EventRepository eventRepository, UserResolver userResolver) {
         this.eventRepository = eventRepository;
+        this.userResolver = userResolver;
     }
 
     public FetcherHandler createHandler(Fetcher<?> fetcher, User user) {
         return new FetcherHandler(fetcher, user, eventRepository);
     }
 
+    public List<FetcherHandler> createHandlers(List<Fetcher<?>> fetchers) {
+        List<FetcherHandler> handlers = new ArrayList<FetcherHandler>();
+        for (User user : userResolver.getAllUsers()) {// TODO we should create handler only for users with account.
+            for (Fetcher<?> fetcher : fetchers) {
+                handlers.add(createHandler(fetcher, user));
+            }
+        }
+        return handlers;
+    }
 }
--- src/test/java/fr/xevents/core/fetcher/FetcherHandlerFactoryTest.java
+++ src/test/java/fr/xevents/core/fetcher/FetcherHandlerFactoryTest.java
@@ -1,24 +1,34 @@
 package fr.xevents.core.fetcher;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.collect.Lists;
+
 import fr.xevents.core.EventRepository;
 import fr.xevents.core.User;
+import fr.xevents.core.UserResolver;
 
 public class FetcherHandlerFactoryTest {
 
     private FetcherHandlerFactory factory;
     private EventRepository eventRepository;
+    private UserResolver userResolver;
 
     @Before
     public void initBeforeTest() throws Exception {
         eventRepository = mock(EventRepository.class);
-        factory = new FetcherHandlerFactory(eventRepository);
+        userResolver = mock(UserResolver.class);
+        factory = new FetcherHandlerFactory(eventRepository, userResolver);
     }
 
     @Test
@@ -32,13 +42,36 @@ public class FetcherHandlerFactoryTest {
     }
 
     @Test
-    public void shouldCreateFetcher() throws Exception {
+    public void shouldCreateHandler() throws Exception {
         Fetcher<?> fetcher = mock(Fetcher.class);
         User user = new User("username");
 
         FetcherHandler handler = factory.createHandler(fetcher, user);
 
         assertThat(handler, notNullValue());
+    }
+
+    @Test
+    public void shouldCreateHandlers() throws Exception {
+        Fetcher<?> fetcher = mock(Fetcher.class);
+        Fetcher<?> fetcher2 = mock(Fetcher.class);
+        when(userResolver.getAllUsers()).thenReturn(Lists.newArrayList(new User("user")));
+
+        List<FetcherHandler> handlers = factory.createHandlers(Lists.newArrayList(fetcher, fetcher2));
+
+        assertThat(handlers, notNullValue());
+        assertThat(handlers.size(), equalTo(2));
+    }
+
+    @Test
+    public void shouldResolveUserToCreateHandlers() throws Exception {
+        Fetcher<?> fetcher = mock(Fetcher.class);
+        Fetcher<?> fetcher2 = mock(Fetcher.class);
+        when(userResolver.getAllUsers()).thenReturn(Lists.newArrayList(new User("user")));
+
+        factory.createHandlers(Lists.newArrayList(fetcher, fetcher2));
+
+        verify(userResolver).getAllUsers();
     }
 
 }
