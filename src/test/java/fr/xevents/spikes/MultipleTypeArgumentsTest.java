408c893670954aa78d80aad59f87ea34539960c4
Benoit Guerout <bguerout@gmail.com>
2011-06-17 07:45:56 +0200

    Changing invalid static import

--- src/test/java/fr/xevents/spikes/MultipleTypeArgumentsTest.java
+++ src/test/java/fr/xevents/spikes/MultipleTypeArgumentsTest.java
@@ -1,7 +1,7 @@
 package fr.xevents.spikes;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.springframework.core.GenericTypeResolver;
