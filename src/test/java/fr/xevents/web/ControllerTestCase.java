0ff32ae3da051988897dc99895652bfbe215720d
bguerout <bguerout@gmail.com>
2011-07-19 17:23:39 +0200

    Moving to spring 3.1.0RC2

--- src/test/java/fr/xevents/web/ControllerTestCase.java
+++ src/test/java/fr/xevents/web/ControllerTestCase.java
@@ -3,20 +3,12 @@ package fr.xevents.web;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/webmvc-config.xml",
-        "classpath*:/spring-tests.xml" })
 public abstract class ControllerTestCase {
 
-    @Autowired
     protected AnnotationMethodHandlerAdapter handlerAdapter;
     protected MockHttpServletRequest request;
     protected MockHttpServletResponse response;
@@ -26,5 +18,6 @@ public abstract class ControllerTestCase {
         initMocks(this);
         request = new MockHttpServletRequest();
         response = new MockHttpServletResponse();
+        handlerAdapter = new AnnotationMethodHandlerAdapter();
     }
 }
