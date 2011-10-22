d41f3f74933e60e3c644bf2e8349ca72a02f0910
Benoit Guerout <bguerout@gmail.com>
2011-10-22 10:11:26 +0200

    Add Account Controller

--- src/main/java/fr/keemto/web/AccountController.java
+++ src/main/java/fr/keemto/web/AccountController.java
@@ -1,4 +1,52 @@
 package fr.keemto.web;
 
+import fr.keemto.core.Account;
+import fr.keemto.core.AccountFactory;
+import fr.keemto.core.User;
+import fr.keemto.core.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/api/accounts")
 public class AccountController {
+
+    public static final int MAX_RANDOM_ACCOUNTS = 5;
+    private final AccountFactory accountFactory;
+    private final UserRepository userRepository;
+    private int maxRandomAccount = MAX_RANDOM_ACCOUNTS;
+
+    @Autowired
+    public AccountController(AccountFactory accountFactory, UserRepository userRepository) {
+        this.accountFactory = accountFactory;
+        this.userRepository = userRepository;
+    }
+
+
+    @RequestMapping(value = "/random", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Account> getRandomAccounts() {
+        List<Account> accounts = new ArrayList<Account>();
+        List<User> users = userRepository.getAllUsers();
+        for (User user : users) {
+            accounts.addAll(accountFactory.getAccounts(user));
+        }
+        return randomize(accounts, Account.class);
+    }
+
+    private <T> List<T> randomize(List<T> accounts, Class<T> clazz) {
+        Collections.shuffle(accounts);
+        return accounts.subList(0, accounts.size() < maxRandomAccount ? accounts.size() : maxRandomAccount);
+    }
+
+    public void setMaxRandomAccount(int maxRandomAccount) {
+        this.maxRandomAccount = maxRandomAccount;
+    }
 }
--- src/test/java/fr/keemto/web/AccountControllerTest.java
+++ src/test/java/fr/keemto/web/AccountControllerTest.java
@@ -1,10 +1,75 @@
 package fr.keemto.web;
 
+import com.google.common.collect.Lists;
+import fr.keemto.TestAccount;
+import fr.keemto.core.*;
+import org.codehaus.jackson.JsonNode;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
-public class AccountControllerTest {
+import java.security.Principal;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class AccountControllerTest extends ControllerTestCase {
+
+    private AccountController controller;
+    private UserRepository userRepository;
+    private AccountFactory accountFactory;
+    private Account account;
+    private User user;
+
+
+    @Before
+    public void initBeforeTest() throws Exception {
+
+        accountFactory = mock(AccountFactory.class);
+        userRepository = mock(UserRepository.class);
+        controller = new AccountController(accountFactory, userRepository);
+
+        request.addHeader("Accept", "application/json");
+        request.setMethod("GET");
+        request.setRequestURI("/api/accounts/random");
+
+        user = new User("test");
+        account = new TestAccount(new AccountKey("provider", "id", user), "", "", "");
+    }
+
     @Test
-    public void testGetRandomAccounts() throws Exception {
+    public void testGetRandomAccountForAUser() throws Exception {
 
+        when(userRepository.getAllUsers()).thenReturn(Lists.newArrayList(user));
+        when(accountFactory.getAccounts(user)).thenReturn(Lists.newArrayList(account));
+        controller.setMaxRandomAccount(1);
+
+        handlerAdapter.handle(request, response, controller);
+
+        assertThat(response.getStatus(), equalTo(200));
+        JsonNode jsonNode = toJsonNode(response.getContentAsString());
+        assertThat(jsonNode.size(), equalTo(1));
+        JsonNode connx = jsonNode.get(0);
+        JsonNode keyNode = connx.get("key");
+        assertThat(keyNode.get("providerId").getValueAsText(), equalTo("provider"));
+    }
+
+
+    @Test
+    public void testGetRandomAccountsForManyUsers() throws Exception {
+
+        when(userRepository.getAllUsers()).thenReturn(Lists.newArrayList(user, user, user));
+        when(accountFactory.getAccounts(user)).thenReturn(Lists.newArrayList(account, account, account));
+        controller.setMaxRandomAccount(2);
+
+        handlerAdapter.handle(request, response, controller);
+
+        assertThat(response.getStatus(), equalTo(200));
+        JsonNode jsonNode = toJsonNode(response.getContentAsString());
+        assertThat(jsonNode.size(), equalTo(2));
     }
 }
