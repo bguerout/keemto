70be711e5808aed53437621e079afa9ef05773d4
Benoit Guerout <bguerout@gmail.com>
2011-10-26 19:46:53 +0200

    Delete old connection controller

--- src/main/java/fr/keemto/web/HashConnectController.java
+++ src/main/java/fr/keemto/web/HashConnectController.java
@@ -1,18 +1,23 @@
 package fr.keemto.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.web.ConnectController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.servlet.view.RedirectView;
 
+@Controller
+@RequestMapping("/connect")
 public class HashConnectController extends ConnectController {
 
     private static final String ACCOUNTS_VIEW = "/#accounts";
 
+    @Autowired
     public HashConnectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
         super(connectionFactoryLocator, connectionRepository);
     }
